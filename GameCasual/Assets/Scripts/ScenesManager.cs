21591a1c0eb277d447976a3fe0b80d2ffa60bcfb
Hapsari Shollulia <71116982+hapsarisho@users.noreply.github.com>
2020-10-02 22:00:34 +0700

    GameCasual

---
+++ GameCasual/Assets/Scripts/ScenesManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ScenesManager : MonoBehaviour
+{
+    public string EnterScene;
+    public string EscapeScene;
+    public bool isEscapeForQuit = false;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+       if (Input.GetKeyUp(KeyCode.Return))
+        {
+            Debug.Log("Name Scene: " + EnterScene);
+            SceneManager.LoadScene(EnterScene);
+        }
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            if (isEscapeForQuit)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                Debug.Log("Name Scene: " + EscapeScene);
+                SceneManager.LoadScene(EscapeScene);
+            }
+        }
+    }
+}
