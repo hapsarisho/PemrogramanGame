7d3d9c236da6d89e444f6154394a32b9b701cb14
Hapsari Shollulia <71116982+hapsarisho@users.noreply.github.com>
2020-10-13 13:17:02 +0700

    MenuGame

---
+++ MenuGame/Assets/Scripts/Loading.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+    [SerializeField] private float delayLoading = 3f;
+    [SerializeField] private string namaScene;
+
+    private float timeElapsed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed > delayLoading)
+        {
+            SceneManager.LoadScene(namaScene);
+        }
+    }
+}
---
+++ MenuGame/Assets/Scripts/Scene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+    public void MenuScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void LoadingScene()
+    {
+        SceneManager.LoadScene("Loading");
+    }
+
+    public void KeluarScene()
+    {
+        Application.Quit();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
