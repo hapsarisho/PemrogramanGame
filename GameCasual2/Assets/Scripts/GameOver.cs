de81af25d7c46cc0f3389b4d92e4604afe9aa5c0
Hapsari Shollulia <71116982+hapsarisho@users.noreply.github.com>
2020-10-16 17:55:53 +0700

    Memilah Sampah

---
+++ GameCasual2/Assets/Scripts/GameOver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    float timer = 0;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 2)
+        {
+            Data.score = 0;
+            SceneManager.LoadScene("Gameplay");
+        }
+    }
+}
