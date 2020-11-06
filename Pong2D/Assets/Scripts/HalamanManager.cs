758fc6d9b7beb3955033784b7bfd290d6198b759
Hapsari Shollulia <71116982+hapsarisho@users.noreply.github.com>
2020-11-06 23:56:43 +0700

    Pong2D

---
+++ Pong2D/Assets/Scripts/HalamanManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HalamanManager : MonoBehaviour
+{
+    public bool isEscapeToExit;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp (KeyCode.Escape))
+        {
+            Application.Quit();
+        } else {
+            KembaliKeMenu();
+        }
+    }
+
+    public void MulaiPermainan()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+    public void KembaliKeMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
